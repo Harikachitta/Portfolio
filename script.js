f8cdac8de5ba0c95e0ac978d42be5e62be2fb15b
Harikachitta <hchitta99@gmail.com>
2025-05-19 13:36:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu()
+{
+    const menu = document.querySelector(".nav-links");
+    const icon = document.querySelector(".desktop-nav");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
